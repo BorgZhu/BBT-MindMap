1a97a624597e4b2d49609b0d1443ae1543feda96
Bjarne Leif Bruhn <bjarne.bruhn@arcor.de>
2013-11-28 14:49:07 +0100

    Initialisiert

---
+++ BBT/AMindMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBT
+{
+    abstract class AMindMap
+    {
+        protected void onAddNode(object sender, ANode node)
+        {
+            if (this.addNodeEvent != null)
+                addNodeEvent(this, node);
+        }
+
+        protected void onRemoveNode(object sender, ANode node)
+        {
+            if (this.removeNodeEvent != null)
+                removeNodeEvent(this, node);
+        }
+
+        public virtual void removeNode(ANode node);
+        public virtual void addNode(ANode node);
+        public virtual delegate void addNodeEventHandler(object sender, ANode node);
+        public delegate void removeNodeEventHandler(object sender, ANode node);
+
+        public event addNodeEventHandler addNodeEvent;
+        public event removeNodeEventHandler removeNodeEvent;
+    }
+}
---
+++ BBT/IStyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BBT
+{
+    interface IStyle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Farbe und True, wenn gefüllt</returns>
+        Tuple<Color, bool> getColor();
+        string getForm();
+    }
+}
