36132ab6d65ca9bf724de8570fcfdcbcd09c4d43
Timo Sochert <T.Sochert@gmail.com>
2013-12-09 16:16:13 +0100

    Added Dreieck.cs Implementation

---
+++ BBT/Dreieck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BBT
+{
+
+    class Dreieck : IForm
+    {
+        System.Windows.Controls.Grid IForm.getStrokeFromNode(ANode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
