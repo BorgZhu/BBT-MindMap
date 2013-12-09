7ce0e445410da69d8cb994cb32732a9fdcc2a800
Timo Sochert <T.Sochert@gmail.com>
2013-12-09 18:26:55 +0100

    Icon.cs erste version

---
+++ BBT/Icon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BBT
+{
+    class Icon : IForm
+    {
+
+        public Grid getStrokeFromNode(ANode node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
