65ebf902101557af115fa3b784dab815a5c19f18
Timo Sochert <T.Sochert@gmail.com>
2013-12-09 16:15:55 +0100

    Added Rechteck implementation

---
+++ BBT/Rechteck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BBT
+{
+
+    class Rechteck : IForm
+    {
+        Grid IForm.getStrokeFromNode(ANode node)
+        {
+            Grid litit = new Grid();
+            litit.Height = node.getRectangle().Height;
+            litit.Width = node.getRectangle().Width;
+
+            Rectangle rectum = new Rectangle();
+            rectum.Fill = new SolidColorBrush(Colors.Black);
+            litit.Children.Add(rectum);
+
+
+
+            return litit;
+        }
+    }
+}
