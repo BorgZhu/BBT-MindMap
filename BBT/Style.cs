eb38585c298d1432d321f7b2e5b3444ba936f707
Timo Sochert <T.Sochert@gmail.com>
2013-12-09 16:13:31 +0100

    Added Style Implementation

---
+++ BBT/Style.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBT
+{
+    class Style : IStyle
+    {
+        Tuple<System.Windows.Media.Color, bool> farbeFill;
+
+        Tuple<System.Windows.Media.Color, bool> IStyle.getColor()
+        {
+            return farbeFill;
+        }
+
+        void IStyle.setColor(Tuple<System.Windows.Media.Color, bool> color)
+        {
+            this.farbeFill = color;
+        }
+    }
+  
+}
