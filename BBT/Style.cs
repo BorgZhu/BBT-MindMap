eeb4b6206ac9a8f8a920352a119f43e99e7e128c
Timo Sochert <T.Sochert@gmail.com>
2013-12-11 12:10:34 +0100

    Style.cs Testversion

--- BBT/Style.cs
+++ BBT/Style.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace BBT
     class Style : AStyle
     {
         Tuple<Color, bool> farbeFill;
+        System.Drawing.Bitmap icon;
 
         public override Tuple<Color, bool>getColor()
         {
@@ -20,6 +22,21 @@ namespace BBT
         {
             this.farbeFill = color;
             changeStyle(this, this);
+        }
+
+        public override System.Drawing.Bitmap getIcon()
+        {
+            return this.icon;
+        }
+
+        public override void setICon(System.Drawing.Bitmap icon)
+        {
+            this.icon = icon;
+        }
+
+        public override void isActivated(bool active)
+        {
+            throw new NotImplementedException();
         }
     }
   
