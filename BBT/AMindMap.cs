c7fbbf100968b1df2c367db7ba914590b30cf821
Bjarne Leif Bruhn <bjarne.bruhn@arcor.de>
2013-12-11 14:23:06 +0100

    API spezifikation geändert für AMindMap.cs

--- BBT/AMindMap.cs
+++ BBT/AMindMap.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace BBT
 {
@@ -20,13 +23,18 @@ namespace BBT
                 removeNodeEvent(this, node);
         }
 
-        public abstract void removeNode(ANode node);
+        public abstract void setMainNode(ANode node);
+        public abstract void removeNode(ANode node, bool recursive = false);
         public abstract void addNode(ANode node);
         public delegate void addNodeEventHandler(object sender, ANode node);
         public delegate void removeNodeEventHandler(object sender, ANode node);
 
         public abstract string toJson();
         public abstract void fromJson(string Json);
+
+        public abstract Tuple<Line, Grid> getDisplay(ANode element);
+        public abstract ANode getMainNode();
+        public abstract Point transformCoords(Point transformPoint, Point mainNodeCoords);
 
         public event addNodeEventHandler addNodeEvent;
         public event removeNodeEventHandler removeNodeEvent;
