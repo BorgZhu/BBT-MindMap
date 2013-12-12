76aa64d75dd71cf98044d4ca819dfb0a94bd3334
Timo Sochert <T.Sochert@gmail.com>
2013-12-12 18:48:48 +0100

    Node.cs erste json implement

--- BBT/Node.cs
+++ BBT/Node.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Ink;
+using System.Web;
+using System.Xml.Linq;
+using System.Windows.Media.Imaging;
 
 namespace BBT
 {
     /// <summary>
     /// Datenklasse für einen Node
     /// </summary>
+    
     class Node : ANode
     {
         Rect grundform;
@@ -19,16 +23,19 @@ namespace BBT
         IStyle stil;
         IForm shape = new Rechteck();
 
+        
         /// <summary>
         /// gibt Rectangle für die Größe des Grid zurück
         /// </summary>
         /// <returns></returns>
         public override Rect getRectangle()
         {
-            //if (grundform != null)
+            if (grundform != null)
                 return grundform;
-            //else
-            //    return new Rect(0, 0, 0, 0);
+            else
+                return new Rect(0, 0, 0, 0);
+
+            
         }
         /// <summary>
         /// setzt die größe des Rectangle der die Größe des Grids bestimmt
@@ -118,13 +125,29 @@ namespace BBT
         /// <returns></returns>
         public override string toJson()
         {
-            throw new NotImplementedException();
+
+            XElement data =
+                new XElement("node",
+                    new XElement("grundform",
+                        new XElement("positionX", "" + grundform.X),
+                        new XElement("positionY", grundform.Y),
+                        new XElement("breite", grundform.Width),
+                        new XElement("hoehe", grundform.Height)
+                        ),
+                    new XElement("text", text),
+                    new XElement("Style",
+                        new XElement("Farbe", stil.getColor().Item1),
+                        new XElement("Filled", stil.getColor().Item2),
+                        new XElement("image", stil.getIcon().BaseUri),
+                        new XElement("Aktive", stil.getActivated()),
+                        new XElement("Fontsize", stil.getFontsize())
+                        ),
+                    new XElement("Form", shape.ToString())
+                    );
+            Console.Write(data.ToString());
+            return data.ToString();
         }
 
-        /// <summary>
-        /// setzt Node aus Json
-        /// </summary>
-        /// <param name="Json"></param>
         public override void fromJson(string Json)
         {
             throw new NotImplementedException();
